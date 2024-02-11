725cd6bc56b5ee96c283e07bc2658b9c79825209
Katuhinx <pismennayaka02@mail.ru>
2024-02-12 02:05:34 +0300

    init version

--- test.c
+++ test.c
@@ -9,9 +9,9 @@
 
 float MAXEPS = 0.5f;
 
-#pragma dvm array distribute [ block ][ block ]
+//#pragma dvm array distribute [ block ][ block ]
 float A [ L ][ M ];
-#pragma dvm array align ([ i ][ j ] with A [ i ][ j ])
+//#pragma dvm array align ([ i ][ j ] with A [ i ][ j ])
 float B [ L ][ M ];
 
 int main ( int an , char ** as )
@@ -20,10 +20,10 @@ int main ( int an , char ** as )
     int i , j , it ;
     float eps ;
 
-    #pragma dvm region
+   // #pragma dvm region
     {
 
-        #pragma dvm parallel ([ i ][ j ] on A [ i ][ j ])
+       // #pragma dvm parallel ([ i ][ j ] on A [ i ][ j ])
         for ( i = 0; i < L ; i ++)
             for ( j = 0; j < M ; j ++)
             {
@@ -39,10 +39,10 @@ int main ( int an , char ** as )
     for ( it = 1; it <= ITMAX ; it ++)
     {
         eps = 0;
-        #pragma dvm actual ( eps )
-        #pragma dvm region
+       // #pragma dvm actual ( eps )
+       // #pragma dvm region
         {
-            #pragma dvm parallel ([ i ][ j ] on A [ i ][ j ]) reduction ( max ( eps ) )
+           // #pragma dvm parallel ([ i ][ j ] on A [ i ][ j ]) reduction ( max ( eps ) )
             for ( i = 1; i < L - 1; i ++)
                 for ( j = 1; j < M - 1; j ++)
                 {
@@ -51,19 +51,19 @@ int main ( int an , char ** as )
                         A [ i ][ j ] = B [ i ][ j ];
                 }
                 
-            #pragma dvm parallel ([ i ][ j ] on A [ i ][ j ]) shadow_renew ( A )
+           // #pragma dvm parallel ([ i ][ j ] on A [ i ][ j ]) shadow_renew ( A )
             for ( i = 1; i < L - 1; i ++)
                 for ( j = 1; j < M - 1; j ++)
                     B [ i ][ j ] = ( A [i -1][ j ] + A [ i ][ j -1] + A [ i ][ j +1] + A [ i +1][ j ]) / 4.0f ;
         }
 
-        #pragma dvm get_actual ( eps )
+       // #pragma dvm get_actual ( eps )
         printf ( " IT = %4i EPS = %14.7E \n " , it , eps ) ;
         if ( eps < MAXEPS )
         break ;
     }
     
-    #pragma dvm get_actual (B)
+   // #pragma dvm get_actual (B)
     printf ("Size = %6d x %6d \n" , L, M ) ;
     printf ("Iterations = %12d\n" , ITMAX ) ;
     printf ("Operation type = floating point \n") ;
